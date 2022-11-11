630b96ebd204828522ce55ed55dcb9c942b39413
AgustinSalazar2 <agustinsssalazar@gmail.com>
2022-11-10 23:27:21 -0300

    Guardado de datos de sesion en el localStorage 10-11-2022

--- src/reducers/authReducer.js
+++ src/reducers/authReducer.js
@@ -3,16 +3,20 @@ import { type } from '../types/type'
 export const authReducer = (state, action) => {
     switch (action.type) {
         case type.login:
-            // JSON.stringify(localStorage.setItem('user',{
-            //     ...action.payload,
-            //     isLogged: true
-            // }))
+            localStorage.setItem('user',JSON.stringify({
+                ...action.payload,
+                isLogged: true
+            }))
             
             return {
                 isLogged: true,
                 ...action.payload
             }
         case type.logout:
+            localStorage.setItem('user',JSON.stringify({
+                isLogged: false
+            }))
+            
             return { isLogged: false }
         default:
             return state
